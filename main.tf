cf9237cd0fd9c43b922c0c4ed4b21a0e23ecb426
Armanshaikh9129 <155525862+Armanshaikh9129@users.noreply.github.com>
2024-03-21 11:26:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.41.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.41"
     }
-  }
-}
 
 provider "aws" {
-  # Configuration options
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "my_ec2_instance" {
-    ami = "ami-0d7a109bf30624c99"
-    instance_type = "t2.micro"
-    key_name = "armankey"
+  ami           = "ami-0d7a109bf30624c99"
+  instance_type = "t2.micro"
+  key_name      = "armankey"
   
+  tags = {
+    Name = "MyEC2Instance"
+  }
 }
